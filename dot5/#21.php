3f2562bb8b1202e6cec64bd712f98d86fd9eb91e
giran-git <giran904587034+git@gmail.com>
2021-10-16 18:47:37 +0900

    usort()で配列をソート

---
+++ dot5/#21.php
@@ -0,0 +1,26 @@
+<?php
+
+// $scores = [
+//   'taguchi' => 80,
+//   'hayashi' => 70,
+//   'kikuchi' => 60,
+// ];
+
+$data = [
+  ['name' => 'taguchi', 'score' => 80],
+  ['name' => 'kikuchi', 'score' => 60],
+  ['name' => 'hayashi', 'score' => 70],
+  ['name' => 'tamachi', 'score' => 60],
+];
+
+usort(
+  $data,
+  function ($a, $b) {
+    if ($a['score'] === $b['score']) {
+      return 0;
+    }
+    return $a['score'] > $b['score'] ? 1 : -1;
+  }
+);
+
+print_r($data) . PHP_EOL;
